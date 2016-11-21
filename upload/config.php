b3f6ad9a0c737a4dc8d71a483d6ff2e7b18960b8
Jardel Santos <jardel@bf2tecnologia.com.br>
2016-11-21 15:53:33 -0200

    Configuração Ambiente Produção

---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://bf2commerce.localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://bf2commerce.localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/BF2/Workspace/Web/bf2commerce/upload/catalog/');
+define('DIR_SYSTEM', 'C:/BF2/Workspace/Web/bf2commerce/upload/system/');
+define('DIR_IMAGE', 'C:/BF2/Workspace/Web/bf2commerce/upload/image/');
+define('DIR_LANGUAGE', 'C:/BF2/Workspace/Web/bf2commerce/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/BF2/Workspace/Web/bf2commerce/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/BF2/Workspace/Web/bf2commerce/upload/system/config/');
+define('DIR_CACHE', 'C:/BF2/Workspace/Web/bf2commerce/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/BF2/Workspace/Web/bf2commerce/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/BF2/Workspace/Web/bf2commerce/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/BF2/Workspace/Web/bf2commerce/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/BF2/Workspace/Web/bf2commerce/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'bf2commerce');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'bf2_');
